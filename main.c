0d95c49ba834a03bb9ed049e20fd0fb6fcc458f2
Alan Leavy <aleavy@tns.org>
2011-01-20 14:24:11 +0300

    Give a personalized Hello.

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 int main(int argc, char **argv)
 {
-	printf("Hello world!\n");
+	char name[255];
+
+	printf("Enter your name: ");
+	fgets(name, 255, stdin);
+	name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+
+	printf("Hello %s!\n", name);
 	return 0;
 }
